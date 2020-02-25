4e537381b53f2af5a96134afb4d71fe1b14ac2bc
Mihail Ivanchev <mihail.ivanchev@bgosoftware.com>
2020-02-25 14:59:43 +0200

    Update .gitignore and get Vue to work.

---
+++ resources/js/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <example-component></example-component>
+    </div>
+</template>
+
+<script>
+    // Imports
+    import ExampleComponent from './components/ExampleComponent';
+
+
+    export default {
+        components: {
+            'example-component': ExampleComponent
+        },
+        data() {
+            return {}
+        },
+        methods: {}
+    }
+</script>
+
+<style>
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,21 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+import App from './App.vue';
+import Vue from 'vue';
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
+window.Vue = require('vue');
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -19,8 +29,6 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -29,4 +37,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    render: h => h(App)
 });
